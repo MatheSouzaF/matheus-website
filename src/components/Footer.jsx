2ca988964a324334170c3780b0314f812249873a
matheussf <matheus@dzigual.com.br>
2025-08-25 13:47:05 -0300

    ajustes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ export const Footer = () => {
   const year = new Date().getFullYear();
 
   return (
-    <div className="py-4 text-zinc-300">
+    <div className="py-4 text-zinc-500">
       <div className="container mx-auto text-center">
         <p>&copy; {year} - Todos os direitos reservados</p>
       </div>
